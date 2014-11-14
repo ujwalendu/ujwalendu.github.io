6aac2be08a29bbae4b61a1316d87196833127d98
ujwalendu <up@MyMacs-MacBook-Pro.local>
2014-11-14 21:45:52 +0530

    LocalStorage Started

--- todo/js/main.js
+++ todo/js/main.js
@@ -9,13 +9,35 @@ todoApp.controller('todoCtrl', ['$scope', function($scope){
 
     if(window.localStorage){
 
-        if(typeof localStorage.uprakash_todoItems === "undefined") {
+        if(typeof localStorage.getItem("todoItems") === "undefined") {
             $scope.todoList = [];
         } else {
 
-            $scope.todoList = localStorage.uprakash_todoItems;
+            $scope.todoList = localStorage.getItem("todoItems");
         }
     }
 
+    $scope.addTodo = function() {
+
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+
+        if(dd<10) {
+            dd='0'+dd
+        }
+
+        if(mm<10) {
+            mm='0'+mm
+        }
+
+        today = dd+'/'+mm+'/'+yyyy;
+        var todoText = $scope.todoText;
+        var todoTime = today;
+
+        $scope.todoList.push({'todoText':todoText, 'todoTime':todoTime});
+        localStorage.setItem("todoItems", $scope.todoList);
+    }
 
 }]);
